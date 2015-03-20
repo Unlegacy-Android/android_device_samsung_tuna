9975f1338d1d092bdd3129f89ccbad0536ff1d58
Kyle Repinski <repinski23@gmail.com>
2015-03-20 14:21:01 -0500

    audio: Add ability to actually force the out stream sampling rate.
    This audio HAL has always required some sort of resampling.
    THIS. ENDS. NOW.

--- audio/Android.mk
+++ audio/Android.mk
@@ -26,5 +26,7 @@ LOCAL_C_INCLUDES += \
 LOCAL_SHARED_LIBRARIES := liblog libcutils libtinyalsa libaudioutils libdl
 LOCAL_MODULE_TAGS := optional
 
+LOCAL_CFLAGS += -DFORCE_OUT_SAMPLING_RATE=44100
+
 include $(BUILD_SHARED_LIBRARY)
 

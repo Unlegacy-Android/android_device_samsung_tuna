862364566c32fea340c3f6bb9db3ccd50e087507
Ziyan <jaraidaniel@gmail.com>
2015-03-26 22:37:04 +0100

    Use GCC 4.7 for the kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,6 +47,7 @@ BOARD_KERNEL_BASE := 0x80000000
 # BOARD_KERNEL_CMDLINE :=
 TARGET_KERNEL_CONFIG := tuna_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/tuna
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7
 
 # Use dlmalloc
 MALLOC_IMPL := dlmalloc

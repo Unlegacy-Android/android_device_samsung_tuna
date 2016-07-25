4c9508ae9614979db2b15fc7c6ec9c239d227e6c
Ziyan <jaraidaniel@gmail.com>
2016-07-25 13:02:19 +0200

    Revert "Set selinux to permissive for now"
    This reverts commit 0853226e61ffc6190dd170f8b0947cb5359dd919.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,7 +32,7 @@ TARGET_OTA_ASSERT_DEVICE := tuna,,maguro,toro,toroplus
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=$(TARGET_BOOTLOADER_BOARD_NAME) androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := androidboot.hardware=$(TARGET_BOOTLOADER_BOARD_NAME)
 TARGET_KERNEL_CONFIG := tuna_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/tuna
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7
